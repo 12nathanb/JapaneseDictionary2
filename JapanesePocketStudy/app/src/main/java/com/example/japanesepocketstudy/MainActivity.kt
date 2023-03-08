83efbc0c05ed8787b960cc80ffbc5adc8f40755f
12nathanb <nathan@thebarnetts.co.uk>
2023-03-08 19:19:00 +0000

    Get Stuff drawing on screen

--- JapanesePocketStudy/app/src/main/java/com/example/japanesepocketstudy/MainActivity.kt
+++ JapanesePocketStudy/app/src/main/java/com/example/japanesepocketstudy/MainActivity.kt
@@ -2,16 +2,35 @@ package com.example.japanesepocketstudy
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.japanesepocketstudy.databinding.ActivityMainBinding
 import com.example.japanesepocketstudy.databinding.FragmentMainBinding
+import com.example.japanesepocketstudy.ui.main.FragmentMain
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: FragmentMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = FragmentMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
--- JapanesePocketStudy/app/src/main/java/com/example/japanesepocketstudy/ui/main/FragmentMain.kt
+++ JapanesePocketStudy/app/src/main/java/com/example/japanesepocketstudy/ui/main/FragmentMain.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.example.japanesepocketstudy.R
 import com.example.japanesepocketstudy.databinding.FragmentMainBinding
 
@@ -20,46 +21,23 @@ class FragmentMain : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
         _binding = FragmentMainBinding.inflate(inflater, container, false)
-        val root = binding.root
 
-        binding.settings1.isClickable = true
-        binding.settings1.setOnClickListener {
-            println("tesst")
-        }
-
-        return root
+        return binding.root
     }
 
-    companion object {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private const val ARG_SECTION_NUMBER = "section_number"
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        @JvmStatic
-        fun newInstance(sectionNumber: Int): FragmentMain {
-            return FragmentMain().apply {
-                arguments = Bundle().apply {
-                    putInt(ARG_SECTION_NUMBER, sectionNumber)
-                }
-            }
+        binding.settings1.setOnClickListener {
+            findNavController().navigate(R.id.action_fragmentMain_to_fragmentSettings)
         }
+
     }
 
     override fun onDestroyView() {
