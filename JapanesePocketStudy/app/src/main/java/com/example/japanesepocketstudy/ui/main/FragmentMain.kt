997b46519a5d83c007f4c94ef1539d4469ee6ec9
12nathanb <nathan@thebarnetts.co.uk>
2023-03-08 21:17:02 +0000

    added buttons

--- JapanesePocketStudy/app/src/main/java/com/example/japanesepocketstudy/ui/main/FragmentMain.kt
+++ JapanesePocketStudy/app/src/main/java/com/example/japanesepocketstudy/ui/main/FragmentMain.kt
@@ -1,9 +1,14 @@
 package com.example.japanesepocketstudy.ui.main
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
@@ -16,6 +21,7 @@ import com.example.japanesepocketstudy.databinding.FragmentMainBinding
 class FragmentMain : Fragment() {
 
     private var _binding: FragmentMainBinding? = null
+    lateinit var sharedPref: SharedPreferences
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -27,15 +33,80 @@ class FragmentMain : Fragment() {
     ): View? {
 
         _binding = FragmentMainBinding.inflate(inflater, container, false)
+        sharedPref = requireContext().getSharedPreferences("sharedPref", Context.MODE_PRIVATE)
 
         return binding.root
     }
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.settings1.setOnClickListener {
             findNavController().navigate(R.id.action_fragmentMain_to_fragmentSettings)
+            binding.settings1.background = context?.getDrawable(R.color.teal_200)
+            binding.kanji.background = context?.getDrawable(R.color.teal_700)
+            binding.jisho.background = context?.getDrawable(R.color.teal_700)
+            binding.sentences.background = context?.getDrawable(R.color.teal_700)
+            sharedPref.edit().putString(getString(R.string.prefs_current_tab), "Settings").apply()
+        }
+
+        binding.kanji.setOnClickListener {
+            binding.settings1.background = context?.getDrawable(R.color.teal_700)
+            binding.kanji.background = context?.getDrawable(R.color.teal_200)
+            binding.jisho.background = context?.getDrawable(R.color.teal_700)
+            binding.sentences.background = context?.getDrawable(R.color.teal_700)
+            sharedPref.edit().putString(getString(R.string.prefs_current_tab), "Kanji").apply()
+        }
+
+        binding.sentences.setOnClickListener {
+            binding.settings1.background = context?.getDrawable(R.color.teal_700)
+            binding.kanji.background = context?.getDrawable(R.color.teal_700)
+            binding.jisho.background = context?.getDrawable(R.color.teal_700)
+            binding.sentences.background = context?.getDrawable(R.color.teal_200)
+            sharedPref.edit().putString(getString(R.string.prefs_current_tab), "Sentences").apply()
+        }
+
+        binding.jisho.setOnClickListener {
+            binding.settings1.background = context?.getDrawable(R.color.teal_700)
+            binding.kanji.background = context?.getDrawable(R.color.teal_700)
+            binding.jisho.background = context?.getDrawable(R.color.teal_200)
+            binding.sentences.background = context?.getDrawable(R.color.teal_700)
+            sharedPref.edit().putString(getString(R.string.prefs_current_tab), "Jisho").apply()
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        requireContext()
+
+        val lanauge = sharedPref.getString(getString(R.string.settings_language), "")
+
+        if (lanauge == getString(R.string.jpn)) {
+            binding.settings1TextJpn.isVisible = true
+            binding.settings1TextEng.isVisible = false
+
+            binding.sentencesTextJpn.isVisible = true
+            binding.sentencesTextEng.isVisible = false
+
+            binding.kanjiTextJpn.isVisible = true
+            binding.kanjiTextEng.isVisible = false
+
+            binding.jishoTextJpn.isVisible = true
+            binding.jishoTextEng.isVisible = false
+        } else {
+            binding.settings1TextJpn.isVisible = false
+            binding.settings1TextEng.isVisible = true
+
+            binding.sentencesTextJpn.isVisible = false
+            binding.sentencesTextEng.isVisible = true
+
+            binding.kanjiTextJpn.isVisible = false
+            binding.kanjiTextEng.isVisible = true
+
+            binding.jishoTextJpn.isVisible = false
+            binding.jishoTextEng.isVisible = true
         }
 
     }
--- JapanesePocketStudy/app/src/main/java/com/example/japanesepocketstudy/ui/main/FragmentSettings.kt
+++ JapanesePocketStudy/app/src/main/java/com/example/japanesepocketstudy/ui/main/FragmentSettings.kt
@@ -1,10 +1,12 @@
 package com.example.japanesepocketstudy.ui.main
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.japanesepocketstudy.R
 import com.example.japanesepocketstudy.databinding.FragmentMainBinding
 import com.example.japanesepocketstudy.databinding.FragmentSettingsBinding
 
@@ -30,9 +32,25 @@ class FragmentSettings : Fragment() {
     ): View? {
 
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
-        val root = binding.root
 
-        return root
+        val sharedPref = context?.getSharedPreferences("sharedPref", Context.MODE_PRIVATE)
+
+
+        binding.switch1.isChecked = sharedPref?.getString(getString(R.string.settings_language), "") == getString(R.string.jpn)
+        binding.switch1.setOnCheckedChangeListener { compoundButton, b ->
+            with (sharedPref?.edit()) {
+                if(b){
+                    this?.putString(getString(R.string.settings_language), getString(R.string.jpn))
+                } else {
+                    this?.putString(getString(R.string.settings_language), getString(R.string.eng))
+                }
+
+                this?.apply()
+            }
+        }
+
+        return binding.root
+
     }
 
     companion object {
